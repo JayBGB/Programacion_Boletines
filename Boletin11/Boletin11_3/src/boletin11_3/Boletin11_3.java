7948e584464090669088190769956807e4a0a362
Borja Garcia Barrera <bgarciabarrera@danielcastelao.org>
2022-12-17 13:21:28 +0100

    Boletin 11 añadido

---
+++ Boletin11/Boletin11_3/src/boletin11_3/Boletin11_3.java
@@ -0,0 +1,10 @@
+package boletin11_3;
+
+public class Boletin11_3 {
+
+    public static void main(String[] args) {
+        Calculos obx = new Calculos();
+       obx.area();
+    }
+    
+}
---
+++ Boletin11/Boletin11_4/src/boletin11_4/Boletin11_4.java
@@ -0,0 +1,10 @@
+package boletin11_4;
+
+public class Boletin11_4 {
+
+    public static void main(String[] args) {
+        Calculos obx = new Calculos();
+        obx.tablas();
+    }
+    
+}
